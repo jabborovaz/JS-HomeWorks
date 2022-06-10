59a032d807a2b7425efd22d192dd08b85d5d8abc
Ashraf <greatman-98@mail.ru>
2022-06-11 00:07:16 +0500

    else string methods

--- js/stringMethods.js
+++ js/stringMethods.js
@@ -2,3 +2,13 @@ let a = "What is your name?";
 console.log(a.charAt(5));
 
 // charAt shows string elems with id of them/ 
+
+let priv = "Hello";
+console.log(priv.concat(" Bro"));
+
+// cancat method cancating strings/
+
+let endd = "I simple love you";
+console.log(endd.endsWith("you"));
+
+// endsWith method checking latest word of string with param and returns true or false/
