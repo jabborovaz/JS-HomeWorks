0dd7849c2c8e805bb090431bb8d46aecec7dbe5e
Ashraf <greatman-98@mail.ru>
2022-06-13 23:56:53 +0500

    task2 of switch

---
+++ js/lesson2/task8.js
@@ -0,0 +1,20 @@
+let grade = Number(prompt("number for grade"));
+switch(grade){
+    case 1:
+        console.log("Плохо");
+    break;
+    case 2:
+        console.log("неудовлетворительно");
+    break;
+    case 3:
+        console.log("удовлетворительно");
+    break;
+    case 4:
+        console.log("хорошо");
+    break;  
+    case 5:
+        console.log("отлично");
+    break;
+    default:
+        console.log("Ошибка")           
+}
