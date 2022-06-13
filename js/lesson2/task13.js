d4b6e6cc862df9ad065d7f883781a43bd592a226
Ashraf <greatman-98@mail.ru>
2022-06-14 00:41:31 +0500

    task1 of for

---
+++ js/lesson2/task13.js
@@ -0,0 +1,5 @@
+let k = Number(prompt("type k"));
+let n = Number(prompt("type n"));
+for(i=0; i<n; i++){
+    console.log(k)
+};
---
+++ js/lesson2/task14.js
@@ -0,0 +1,6 @@
+// let a = Number(prompt("type a"));
+// let b = Number(prompt("type b"));
+// let n;
+// for(i=a; i<b; i++){
+//     n++
+// }
