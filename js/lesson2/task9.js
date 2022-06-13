83def9c8027ef9304e192605385d1f592b3fddef
Ashraf <greatman-98@mail.ru>
2022-06-14 00:03:44 +0500

    task3 of switch

---
+++ js/lesson2/task9.js
@@ -0,0 +1,41 @@
+let season = Number(prompt("choose number of mounth"));
+switch(season){
+    case 1:
+        console.log("Winter");
+    break;
+    case 2:
+        console.log("Winter");
+    break;
+    case 3:
+        console.log("Spring");
+    break;
+    case 4:
+        console.log("Spring");
+    break;
+    case 5:
+        console.log("Spring");
+    break;
+    case 6:
+        console.log("Summer");
+    break;
+    case 7:
+        console.log("Summer");
+    break;
+    case 8:
+        console.log("Summer");
+    break;
+    case 9:
+        console.log("Autumn");
+    break;
+    case 10:
+        console.log("Autumn");
+    break;
+    case 11:
+        console.log("Autumn");
+    break;
+    case 12:
+        console.log("Winter");
+    break;
+    default:
+        console.log("There are 12 mounth in a year")                
+};
