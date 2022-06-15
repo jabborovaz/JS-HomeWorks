0754a5a3f4fe08db6c2c99a24432cf070de5222c
jabborovaz <greatman-98@mail.ru>
2022-06-15 08:28:26 +0500

    task1 without reverse

--- js/lesson2/methodTask1.js
+++ js/lesson2/methodTask1.js
@@ -1,4 +1,11 @@
-let world = "world";
-let worldMass = world.split("");
-let naoborot = worldMass.reverse();
-console.log(naoborot.join(""));
+// let world = "world";
+// let worldMass = world.split("");
+// let naoborot = worldMass.reverse();
+// console.log(naoborot.join(""));
+
+let str = "world";
+let str2 = "";
+for(i=str.length-1; i>=0; i--){
+    str2+=str.charAt(i)
+}
+console.log(str2);
