1de43f0f5e722db8b0e8aecc7edf2450a3519a8f
Ashraf <greatman-98@mail.ru>
2022-06-16 07:05:45 +0500

    if with and or

---
+++ js/lesson2/testInSchool.js
@@ -0,0 +1,9 @@
+let clothe = prompt("do yo have clothe: type yes or not");
+let tie = prompt("do you have tie: type yes or not");
+let bag = prompt("do you have bag: type yes or not");
+if(clothe=="yes" && tie=="yes" && bag=="yes"){
+    console.log("Welcome");
+}
+else{
+    console.log("Good Bye");
+};
