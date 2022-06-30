f6637ee86d3b495276da3b6bdb7b7e43eee276a1
Ashraf <greatman-98@mail.ru>
2022-06-30 10:48:29 +0500

    factorial with dom

--- js/lesson 6/dom.js
+++ js/lesson 6/dom.js
@@ -1,19 +1,19 @@
-let redBox = document.querySelector(".red-box");
-let textInp = document.querySelector(".text-form");
-let btn = document.querySelector(".btn");
-let table = document.createElement("table");
-let thead = document.createElement("thead");
-let td = document.createElement("td");
-let tr = document.createElement("tr");
-redBox.append(table);
-table.append(thead);
-thead.innerText = "this table";
-table.append(td);
-td.innerText = "name";
-td.append(tr);
-tr.classList.add("table-radius");
-console.log(tr)
-tr.innerText = "John";
+// let redBox = document.querySelector(".red-box");
+// let textInp = document.querySelector(".text-form");
+// let btn = document.querySelector(".btn");
+// let table = document.createElement("table");
+// let thead = document.createElement("thead");
+// let td = document.createElement("td");
+// let tr = document.createElement("tr");
+// redBox.append(table);
+// table.append(thead);
+// thead.innerText = "this table";
+// table.append(td);
+// td.innerText = "name";
+// td.append(tr);
+// tr.classList.add("table-radius");
+// console.log(tr)
+// tr.innerText = "John";
 
 // btn.onclick = ()=>{
 //     redBox.style.backgroundColor = "red";
@@ -25,7 +25,26 @@ tr.innerText = "John";
 //     }
 // };
 
+// btn.onclick = () =>{
+//     let nam = textInp.value;
+//     alert(`hello ${nam}`)
+// };
+
+let btn = document.querySelector(".btn");
+let inp = document.querySelector(".inp");
+let res = document.querySelector(".res");
+
 btn.onclick = () =>{
-    let nam = textInp.value;
-    alert(`hello ${nam}`)
+    function fact(n){
+        let cont = 1
+        for(i=1; i<=n; i++){
+            cont=cont*i
+        }
+        return cont
+    }
+    let result = fact(inp.value);
+
+    res.innerHTML = result
+
+    
 };
